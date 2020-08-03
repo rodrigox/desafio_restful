3acafac49abf01f712fb125240cd718756a3343c
Rodrigo <rdrigoalex@gmail.com>
2020-08-02 21:38:52 -0300

    Ajuste de path url  no websecurity

--- src/main/java/com/pitang/desafio/ws/AboutUserService.java
+++ src/main/java/com/pitang/desafio/ws/AboutUserService.java
@@ -22,7 +22,6 @@ import com.pitang.desafio.model.AuthenticationRequest;
 import com.pitang.desafio.model.AuthenticatiorResponse;
 import com.pitang.desafio.model.Usuario;
 
-@RequestMapping("carsystem/api")
 @RestController
 public class AboutUserService {
 	
